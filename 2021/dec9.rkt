eabb7290507f8ac9d5d630ef49ea7ad08cd2f25d
Marc Kaufmann <marc.kaufmannmk@gmail.com>
2021-12-09 19:45:04 +0100

    Day 9 solutions

--- 2021/dec9.rkt
+++ 2021/dec9.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
 (require racket/file
+         racket/list
          racket/string)
 
 (define (string->vector s)
@@ -48,18 +49,19 @@
       (if (is-low-point? i j)
           (cons (list i j) low-points)
           low-points)))
+  ;(displayln low-points)
   (define (in-map? ij)
     (define-values (i j) (values (car ij) (cadr ij)))
-    (displayln (list "i: in-map" i))
-    (and (> i 0) (> j 0) (< i h) (< j w)))
+    ;(displayln (list "i: in-map" i))
+    (and (> i -1) (> j -1) (< i h) (< j w)))
   (define (in-basin? ij)
     (< (loc (car ij) (cadr ij)) 9))
   (define (visited? ij)
     (vector-ref (vector-ref cave-map-visited (car ij)) (cadr ij)))
   (define (neighbours ij)
-    (displayln (list "ij: " ij))
+    ;(displayln (list "ij: " ij))
     (define-values (i j) (values (car ij) (cadr ij)))
-    (displayln (list "i: " i))
+    ;(displayln (list "i: " i))
     (filter (lambda (ij)
               (and (in-map? ij) (in-basin? ij) (not (visited? ij))))
             (list (list (sub1 i) j)
@@ -70,25 +72,30 @@
   (define cave-map-visited
     (for/vector ([i h])
       (make-vector w #f)))
-  (define (visit! ns)
-    (displayln (list "ns: " ns))
-    (for ([n ns])
-      (define-values (i j) (values (car n) (cadr n)))
-      (vector-set! (vector-ref cave-map-visited i) j  #t)))
+  (define (visit! ij)
+    ;(displayln (list "ns: " ns))
+    (define-values (i j) (values (car ij) (cadr ij)))
+    (vector-set! (vector-ref cave-map-visited i) j  #t))
   (define (get-basin ij)
     ; returns the size of the basin starting at (i, j) and changes cave-map-visited
     (define (visit-basin next-visit size)
-      (displayln (list "next-visit: " next-visit "size: " size))
+      ;(displayln (list "next-visit: " next-visit "size: " size))
       (cond [(null? next-visit) size]
             [else
              (define ng (neighbours (car next-visit)))
-             (displayln (list "neigh: " ng))
-             (visit! ng)
-             (visit-basin (append (cdr next-visit) ng)
+             ;(displayln (list "neigh: " ng "visiting ngs of " (car next-visit)))
+             (visit! (car next-visit))
+             (visit-basin (remove-duplicates
+                           (append (cdr next-visit) ng))
                           (add1 size))]))
     (visit-basin (list ij) 0))
-  (for/list ([ij low-points])
-    (get-basin ij)))
+  (apply *
+         (take
+          (sort (for/list ([ij low-points])
+    ;(displayln "=======")
+                  (get-basin ij))
+                >)
+          3)))
 
 ; Make a new map of basin-labels for the cave-map
 ; Initially set to nothing (or -1). Go through all. If not yet in a basin, start a new basin there and then fill the basin by walking to all neighbours and labeling them to, while counting the size of the basin. Then repeat (after storing size of basin).
